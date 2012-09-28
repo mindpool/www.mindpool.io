075d26d4966acd6f8a8c694e3f628d28003b26be
Duncan McGreggor <oubiwann@mindpool.io>
2012-09-28 16:22:37 -0700

    Added content module for the splash page.

---
+++ mindpoolsite/content/splash.py
@@ -0,0 +1,12 @@
+consulting = ""
+
+
+training = ""
+
+
+teams = """
+Our teams are a new take on providing cloud technology services. We've got an
+approach that will give you a final product with a smile. But just as
+importantly, you'll be smiling the whole time our teams are working with your
+teams.
+"""
