a88597bd260834e673d9152ee7c3d07c2b7e743a
Duncan McGreggor <oubiwann@mindpool.io>
2012-10-06 19:19:34 -0700

    Added some content for the open source page.

--- mindpoolsite/content/opensource.py
+++ mindpoolsite/content/opensource.py
@@ -1,3 +1,21 @@
 opensource = """
-Like 
+Open Source
+-----------
+
+The members of MindPool have been users of open source software since the
+mid-80s, before the term was even used. By the mid-90s, many of us were deeply
+involved with Linux. As the number of machines on the Web increased and online
+communications became more prevalent, open source projects begun to emerge from
+shared need. We were there. This is where we have been, ever since.
+
+Open source ideologies are at the core of our business. We build what you need.
+We train your staff so that the knowledge becomes a part of your organization.
+One day at a time, the world becomes a more technically knowledgeable place.
+
+All the code that we work on is available on github. These are our `sponsored
+projects`_. Feel free to use what we have created, or base your next project on
+one of ours.
+
+.. Links:
+.. _sponsored projects: http://github.com/mindpool
 """
