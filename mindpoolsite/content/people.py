5683fa43e1528c3d1cc68a6624f6e403903d0018
Duncan McGreggor <oubiwann@mindpool.io>
2012-10-07 10:41:38 -0700

    Added content for teams in the people section.

--- mindpoolsite/content/people.py
+++ mindpoolsite/content/people.py
@@ -1,7 +1,26 @@
 people = ""
 
 
-teams = ""
+teams = """
+Teams
+-----
+
+[DRAFT]
+
+We're not a shop for bodies. We don't provide fleets of robots for use in
+assembly lines software development. We are a close-knit team of software and
+systems engineers that love working with each other, love tackling hard
+problems, and most of all, love delivering world-class integration.
+
+Our teams nurture not only technical excellence, but also close connections
+between all individuals working together, and by estension, everyone in our
+company and our contacts with you.
+
+When you contract for our services, you are getting engineers that know how to
+communicate, know how to engage, know the meaning of strong community
+relationships, and how to bring all this -- combined with their technical
+excellence -- to bear on providing exactly the solution you need.
+"""
 
 
 members = ""
