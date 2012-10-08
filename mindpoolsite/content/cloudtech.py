66356534df0dea8450144eb95619bb05c8696f95
Duncan McGreggor <oubiwann@mindpool.io>
2012-10-07 20:24:20 -0700

    Added new content for cloud tech pages.

--- mindpoolsite/content/cloudtech.py
+++ mindpoolsite/content/cloudtech.py
@@ -1,17 +1,78 @@
-cloudtech = ""
+cloudtech = """
+Cloud Technologies
+------------------
 
-langs = ""
+Ah, the ever-present buzzword, "cloud." Despite the protest of armies of
+systems engineers, it's a useful term. It certainly saves a lot of time typing!
+In the left menu we've outlined only *some* of the technologies encapsulated in
+the term "cloud", and it's still a lot.
 
-frameworks = ""
+Here's what cloud means to us in a nutshell: **the systematic engineering of
+networked resources**. That's it. And this is what we bring you.
 
-concurrency = ""
+Of course, there's a bunch involved in that one phrase. Instead of the usual
+litany of technologies-cum-buzzwords, here's a deeper look that keeps the focus
+on the concepts involved:
 
-messaging = ""
+* packaging of software and systems such that it's installable by one or more
+  configuration management systems
 
-distributed = ""
+* deployment of said resources
 
-data = ""
+* maintenance and optimization of said resources
 
-aas = ""
+* engineering repeatable, re-deployable customizations (e.g., integration,
+  bespoke components, etc.)
 
-ux = ""
+In the subsequent pages of this section, we give examples of the expertise we
+have and how you can leverage this.
+"""
+
+langs = """
+Programming Languages are the Foundation
+----------------------------------------
+
+"""
+
+frameworks = """
+Quicker Deliveries with Libraries and Frameworks
+------------------------------------------------
+
+"""
+
+concurrency = """
+Do More with Concurrency
+------------------------
+
+"""
+
+messaging = """
+Integrate Systems with Messaging
+--------------------------------
+
+"""
+
+distributed = """
+Safeguard with Distributed Systems
+----------------------------------
+
+"""
+
+
+data = """
+Mine Your Data
+--------------
+
+"""
+
+aas = """
+Infrastructure and Platforms as Services
+----------------------------------------
+
+"""
+
+ux = """
+The User Experience in the Cloud
+--------------------------------
+
+"""
