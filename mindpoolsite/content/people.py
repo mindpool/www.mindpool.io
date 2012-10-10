87e82b4a13193c99fa3545700d24baa56dcb8a06
Duncan McGreggor <oubiwann@mindpool.io>
2012-10-10 14:00:28 -0700

    Tweaked team content.

--- mindpoolsite/content/people.py
+++ mindpoolsite/content/people.py
@@ -9,23 +9,21 @@ teams = """
 Teams
 -----
 
-[DRAFT]
+We're not a shop for bodies. We don't provide fleets of drone workers for use
+in assembly-line software dev sweatshops. We are a close-knit team of software
+and systems engineers that love working with each other, love tackling hard
+problems, and most of all, have a passion for delivering world-class solutions.
 
-We're not a shop for bodies. We don't provide fleets of robots for use in
-assembly-line software dev sweatshops. We are a close-knit team of software and
-systems engineers that love working with each other, love tackling hard
-problems, and most of all, with ha passion for delivering world-class
-integration.
-
-Our teams nurture technical excellence in each engineer. As such, you know
-you're getting dedication when you bring us on board. Furthermore, we thrive on
-our close connections with each other and with the teams that we join for the
-duration of our contracts.
+Our teams nurture technical excellence in each member of the team. As such, you
+know you're getting dedication when you bring us on board. Furthermore, we
+thrive on our close connections with each other and with the teams that we join
+for the duration of our contracts. This keeps project momentum strong, even
+during crunch times.
 
 When you hire our services, you are getting engineers that know how to
 communicate, know how to engage, know the meaning of strong community
-relationships, and how to bring all this -- combined with technical excellence
--- to bear on providing exactly the solution you need.
+relationships, and how to bring all this -- combined with our technical
+excellence -- to bear on providing exactly the solution you need.
 """
 
 
