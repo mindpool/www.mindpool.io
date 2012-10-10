367a25d726b2a4266911d9b0dd6c0555a588c9e4
Duncan McGreggor <oubiwann@mindpool.io>
2012-10-10 12:35:56 -0700

    Started adding content for services.

--- mindpoolsite/content/services.py
+++ mindpoolsite/content/services.py
@@ -4,8 +4,22 @@ Services
 
 *Open Source, Delivered*
 
+MindPool provides services oriented around the following converting
+technologies:
 
-TBD
+* open cloud platforms such as OpenStack and CloudFoundry
+
+* software defined networking
+
+* big data
+
+Read more about how we can help you in these areas in our `Consulting`_ and
+`Training`_ pages.
+
+
+.. Links
+.. _Consulting: /services/consulting
+.. _Training: /services/training
 
 """
 
