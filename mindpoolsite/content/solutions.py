bac1092d82874582dc3ca92de789887761615e14
Murali Raju <murali.raju@appliv.com>
2012-10-11 09:33:14 -0400

    Renamed Cloud Technologies to Solutions which is a much better fit

---
+++ mindpoolsite/content/solutions.py
@@ -0,0 +1,137 @@
+solutions = """
+Solutions
+------------------
+
+Ah, the ever-present buzzword, "cloud." Despite the protest of armies of
+systems engineers, it's a useful term. It certainly saves a lot of time typing!
+In the left menu we've outlined only *some* of the technologies encapsulated in
+the term "cloud", and it's still a lot.
+
+Here's what cloud means to us in a nutshell: **the systematic engineering of
+networked resources**. That's it. And this is what we bring you.
+
+Of course, there's a bunch involved in that one phrase. Instead of the usual
+litany of technologies-cum-buzzwords, here's a deeper look that keeps the focus
+on the concepts involved:
+
+* packaging of software and systems such that it's installable by one or more
+  configuration management systems
+
+* deployment of said resources
+
+* maintenance and optimization of said resources
+
+* engineering repeatable, re-deployable customizations (e.g., integration,
+  bespoke components, etc.)
+
+In the subsequent pages of this section, we give examples of the expertise we
+have and how you can leverage this.
+"""
+
+langs = """
+Programming Languages are the Foundation
+----------------------------------------
+
+TBD
+
+*A selection of supported languages:*
+
+=================================================  =================================================
+.. image:: /static/img/python-logo-x153-v3-TM.jpg  .. image:: /static/img/made-with-lisp-x153.jpg
+.. image:: /static/img/ruby-logo-R-x153.jpg        .. image:: /static/img/coffeescript_logo_x153.jpg
+=================================================  =================================================
+"""
+
+frameworks = """
+Quicker Deliveries with Frameworks
+----------------------------------
+
+TBD
+
+*A selection of supported frameworks:*
+
+====================================================  ========================================
+.. image:: /static/img/Twisted_Logo_x153.jpg          .. image:: /static/img/zmq-x153.jpg
+.. image:: /static/img/django-logo-negative-x153.jpg  .. image:: /static/img/knockout-x153.jpg
+====================================================  ========================================
+"""
+
+concurrency = """
+Do More with Concurrency
+------------------------
+
+TBD
+"""
+
+messaging = """
+Integrate Systems with Messaging
+--------------------------------
+
+TBD
+
+*A selection of supported messaging solutions:*
+
+========================================  ===================================
+.. image:: /static/img/RabbitMQ-x153.jpg  .. image:: /static/img/zmq-x153.jpg
+========================================  ===================================
+"""
+
+distributed = """
+Safeguard with Distributed Systems
+----------------------------------
+
+TBD
+"""
+
+
+data = """
+Mine Your Data
+--------------
+
+TBD
+
+*A selection of supported databases:*
+
++-------------------------------------------+------------------------------------------+
+| .. image:: /static/img/riak-x153.jpg      | .. image:: /static/img/mongodb-x153.jpg  |
++-------------------------------------------+------------------------------------------+
+| .. image:: /static/img/cassandra-x153.jpg | .. image:: /static/img/postgres-x153.jpg |
++-------------------------------------------+                                          |
+| .. image:: /static/img/hadoop-x153.jpg    |                                          |
++-------------------------------------------+------------------------------------------+
+"""
+
+aas = """
+Infrastructure and Platforms as Services
+----------------------------------------
+
+TBD
+
+*A selection of supported cloud platforms:*
+
+=============================================  =============================================
+.. image:: /static/img/OpenStackLogo-x153.jpg  .. image:: /static/img/cloud-foundry-x153.jpg
+=============================================  =============================================
+"""
+
+sdn = """
+Software-Defined Networking
+---------------------------
+
+TDB
+
+*A selection of supported SDN solutions:*
+
++-----------------------------------------------------+-----------------------------------------------+
+| .. image:: /static/img/midokura-x153.jpg            | .. image:: /static/img/vmware-nicira-x153.jpg |
++-----------------------------------------------------+                                               |
+| .. image:: /static/img/Big-Switch-networks-x153.jpg |                                               |
++-----------------------------------------------------+-----------------------------------------------+
+"""
+
+ux = """
+The User Experience in the Cloud
+--------------------------------
+
+TBD
+"""
