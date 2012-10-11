6e1b3575c9b556093afaa2f49fecc6c49fc75f51
Murali Raju <murali.raju@appliv.com>
2012-10-11 10:28:23 -0400

    Content updates

--- mindpoolsite/content/solutions.py
+++ mindpoolsite/content/solutions.py
@@ -2,37 +2,45 @@ solutions = """
 Solutions
 ------------------
 
-Ah, the ever-present buzzword, "cloud." Despite the protest of armies of
-systems engineers, it's a useful term. It certainly saves a lot of time typing!
-In the left menu we've outlined only *some* of the technologies encapsulated in
-the term "cloud", and it's still a lot.
+We are able to assemble various solutions based on our Client needs built
+from Open Source foundations. Our current solutions fall into popular implementations
+such as Cloud, Software-Defined-Networking, Big Data as well as building custom solutions
+around messaging, concurrency and machine learning problems for our Clients.
 
-Here's what cloud means to us in a nutshell: **the systematic engineering of
-networked resources**. That's it. And this is what we bring you.
-
-Of course, there's a bunch involved in that one phrase. Instead of the usual
-litany of technologies-cum-buzzwords, here's a deeper look that keeps the focus
-on the concepts involved:
-
-* packaging of software and systems such that it's installable by one or more
-  configuration management systems
-
-* deployment of said resources
-
-* maintenance and optimization of said resources
-
-* engineering repeatable, re-deployable customizations (e.g., integration,
-  bespoke components, etc.)
-
-In the subsequent pages of this section, we give examples of the expertise we
-have and how you can leverage this.
 """
+
+
+# Ah, the ever-present buzzword, "cloud." Despite the protest of armies of
+# systems engineers, it's a useful term. It certainly saves a lot of time typing!
+# In the left menu we've outlined only *some* of the technologies encapsulated in
+# the term "cloud", and it's still a lot.
+
+# Here's what cloud means to us in a nutshell: **the systematic engineering of
+# networked resources**. That's it. And this is what we bring you.
+
+# Of course, there's a bunch involved in that one phrase. Instead of the usual
+# litany of technologies-cum-buzzwords, here's a deeper look that keeps the focus
+# on the concepts involved:
+
+# * packaging of software and systems such that it's installable by one or more
+#   configuration management systems
+
+# * deployment of said resources
+
+# * maintenance and optimization of said resources
+
+# * engineering repeatable, re-deployable customizations (e.g., integration,
+#   bespoke components, etc.)
+
+# In the subsequent pages of this section, we give examples of the expertise we
+# have and how you can leverage this.
+
 
 langs = """
 Programming Languages are the Foundation
 ----------------------------------------
 
-TBD
+We build upon the fundamental building blocks - Open Source languages. 
 
 *A selection of supported languages:*
 
