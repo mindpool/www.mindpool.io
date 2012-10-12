ef29dd2ea314fd8a08806dfc747a4947c95bc3ab
Duncan McGreggor <oubiwann@mindpool.io>
2012-10-12 08:54:08 -0700

    Reworked the "invest in the people" bit.

--- mindpoolsite/content/people.py
+++ mindpoolsite/content/people.py
@@ -2,19 +2,25 @@ people = """
 People
 ------
 
-MindPool is a people company. We invest in our people as they are the company.
+MindPool is a people company. Through best practices in engineering and
+management, we continually focus our attention on what is best for our people.
+This keeps the company fresh and our staff highly motivated; and this is what
+you get from us.
 
 We're not a shop for bodies. We don't provide fleets of drone workers for use
-in assembly-line software dev sweatshops. We are a close-knit team of software
-and systems engineers covering three decades of industry experience who love working 
-with each other, love tackling hard problems, and most of all, have a passion for 
-delivering world-class solutions.
+in assembly-line software dev sweatshops.
+
+We are a close-knit team of software and systems engineers covering three
+decades of industry experience who love working with each other, love tackling
+hard problems, and most of all, have a passion for delivering world-class
+solutions.
 """
 
 
 teams = """
 Teams
 -----
+
 Our teams nurture technical excellence in each member of the team. As such, you
 know you're getting dedication when you bring us on board. Furthermore, we
 thrive on our close connections with each other and with the teams that we join
